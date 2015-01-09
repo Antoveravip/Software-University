4e85ad5e0455b0c0013ca8db34d0221746f57fd3
Antoveravip <petar@petarivanov.com>
2015-01-10 01:10:59 +0200

    Registration almost done
    Registration almost done

---
+++ AngularJS/AdsSystem/app/js/services/data/userData.js
@@ -0,0 +1,31 @@
+﻿adsApp.factory('userData', ['$resource', 'baseServiceUrl', 'authentication', function ($resource, baseServiceUrl, authentication) {
+    var serviceUrl = 'user';
+    
+    function registerUser(user) {
+        return $resource(baseServiceUrl + serviceUrl + "/register")
+            .save(user)
+            .$promise
+            .then(function (data) { 
+                authentication.saveUser(angular.toJson(data));        
+             });
+    }
+    
+    function loginUser(user) { 
+    
+    }
+    
+    function logoutUser(user) { 
+    
+    }
+
+    function getAllUsers() {
+        return resource.query();
+    }
+    
+    return {
+        register: registerUser,
+        login: loginUser,
+        logout: logoutUser,
+        getUsers: getAllUsers
+    };
+}]);
