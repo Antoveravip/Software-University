cb5a3563223b5d9e0d8574c52b3aaa93aee3c36b
Antoveravip <petar@petarivanov.com>
2015-01-10 20:56:10 +0200

    Project structure refactoring

---
+++ AngularJS/AdsSystem/app/scripts/controllers/PublicAdsController.js
@@ -0,0 +1,28 @@
+﻿adsApp.controller('PublicAdsController', ['$scope', 'adsData', 'filter', function ($scope, adsData, filter) {
+    $scope.ready = false;
+    
+    $scope.currentPage = 1;
+    $scope.startPage = 1;
+    $scope.pageSize = 5;
+
+    function loadAds(params) {
+        params = params || {};
+
+        adsData.getAds(params)
+            .$promise
+            .then(function (data) {
+                $scope.adsData = data;
+                $scope.ready = true;
+            });	    
+    }
+
+    loadAds();
+
+    $scope.$on('categoryClicked', function (event, category) { 
+        loadAds(filter.getParams());
+    });
+
+    $scope.$on('townClicked', function (event, town) {
+        loadAds(filter.getParams());
+    });
+}]);
